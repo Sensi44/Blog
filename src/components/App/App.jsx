b0fe47afe9c8e6a14309df8c0df2e887750ac3ac
John <mur.sensi@yandex.ru>
2022-09-16 22:56:04 +0300

    a
    a

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,11 +3,7 @@ import React from 'react';
 import './App.scss';
 
 function App(props) {
-  return (
-    <div className='app'>
-asd
-    </div>
-  );
+  return <div className='app'>asd</div>;
 }
 
 export default App;
